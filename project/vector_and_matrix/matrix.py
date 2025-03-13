cc9bb24413e4709b6b65295a2ca8140db1aff7e1
Kirill-ML19 <kirillvoronkov896@gmail.com>
2025-03-13 06:19:33 +0300

    Fix files 'matrix.py' and 'using_module_matrix' ; Create files for tests

--- project/vector_and_matrix/matrix.py
+++ project/vector_and_matrix/matrix.py
@@ -1,27 +1,38 @@
-from typing import List , Union 
+from typing import List, Union
 
-def matrix_addition(mat1: List[List[Union[float,int]]], mat2: List[List[Union[float,int]]])->List[List[Union[float,int]]]:
+
+def matrix_addition(
+    mat1: List[List[Union[float, int]]], mat2: List[List[Union[float, int]]]
+) -> List[List[Union[float, int]]]:
     """
     Function adds matrices
     """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         raise ValueError("")
-    return [[mat1[i][j]+mat2[i][j]] for j in range(len(mat1[0])) for i in range(len(mat1))]
+    return [
+        [mat1[i][j] + mat2[i][j]] for j in range(len(mat1[0])) for i in range(len(mat1))
+    ]
 
-def matrix_multiplication(mat1: List[List[Union[float,int]]], mat2: List[List[Union[float,int]]])->List[List[Union[float,int]]]:
+
+def matrix_multiplication(
+    mat1: List[List[Union[float, int]]], mat2: List[List[Union[float, int]]]
+) -> List[List[Union[float, int]]]:
     """
     Function multiples matrix
     """
     if len(mat1[0]) != len(mat2):
-        raise ValueError("")
-    result=[[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+        raise ValueError("The number of columns in the first matrix and rows in the second matrix must be the same")
+    result: List[List[Union[float,int]]]=[[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
-                result[i][j]+=mat1[i][k] * mat2[k][j]
+                result[i][j] += mat1[i][k] * mat2[k][j]
     return result
 
-def matrix_transpose(mat: List[List[Union[float,int]]])-> List[List[Union[float,int]]]:
+
+def matrix_transpose(
+    mat: List[List[Union[float, int]]],
+) -> List[List[Union[float, int]]]:
     """
     Function that a transpose matrix
     """
