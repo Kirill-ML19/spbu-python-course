81f693d62b5fb631062d341acfd7d97cea103cc1
Kirill-ML19 <kirillvoronkov896@gmail.com>
2025-03-16 07:26:18 +0300

    Fixed functions for working with vectors
    - Fixed typos in function names:  → ,  → .
    - Improved type annotations: replaced  with .
    - Added detailed docstrings describing functions, input parameters, return results, and possible exceptions.
    - Added check for zero angles in angular_between_vectors function.

--- project/vector_and_matrix/vector.py
+++ project/vector_and_matrix/vector.py
@@ -1,32 +1,62 @@
 import math
-from typing import List, Union
+from typing import List
 
 
-def dot_product(
-    vec1: List[Union[float, int]], vec2: List[Union[float, int]]
-) -> Union[float, int]:
+def dot_product(vec1 : List[float] ,vec2: List[float] ):
+    
     """
-    Function calculates the dot product
+    Calculates the dot product of two vectors.
+
+    Args:
+        vec1 (List[float]): The first vector.
+        vec2 (List[float]): The second vector.
+
+    Returns:
+        float: The dot product of the two vectors.
+
+    Raises:
+        ValueError: If the vectors are not of the same length.
     """
+
     if len(vec1) != len(vec2):
         raise ValueError("Vectors must be the same length")
     return sum(x * y for x, y in zip(vec1, vec2))
 
 
-def vector_lenght(vec: List[Union[float, int]]) -> float:
+def vector_length(vec: List[float]) -> float:
+    
     """
-    Function caclulates the length of a vector
+    Calculates the length (magnitude) of a vector.
+
+    Args:
+        vec (List[float]): The vector.
+
+    Returns:
+        float: The length of the vector.
     """
+
     return math.sqrt(sum(x**2 for x in vec))
 
 
-def angle_betweeen_vectors(
-    vec1: List[Union[float, int]], vec2: List[Union[float, int]]
-) -> float:
+def angle_between_vectors(vec1: List[float] , vec2: List[float]) -> float:
+    
     """
-    Function calculates the angle between vectors
+    Calculates the angle between two vectors in radians.
+
+    Args:
+        vec1 (List[float]): The first vector.
+        vec2 (List[float]): The second vector.
+
+    Returns:
+        float: The angle between the vectors in radians.
+
+    Raises:
+        ValueError: If either vector has zero length.
     """
+
     dot = dot_product(vec1, vec2)
-    len1 = vector_lenght(vec1)
-    len2 = vector_lenght(vec2)
+    len1 = vector_length(vec1)
+    len2 = vector_length(vec2)
+    if len1==0 or len2==0:
+        raise ValueError("Vectors must not have zero length")
     return math.acos(dot / (len1 * len2))
