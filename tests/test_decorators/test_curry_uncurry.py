a4e81955c3155c6b455f94cd8a6b1e7690b5adb6
Kirill-ML19 <kirillvoronkov896@gmail.com>
2025-03-16 12:16:21 +0300

    added tests for uncurry_explicit function

--- tests/test_decorators/test_curry_uncurry.py
+++ tests/test_decorators/test_curry_uncurry.py
@@ -1,56 +1,132 @@
-import pytest 
+import pytest
 from typing import Callable
-from project.Decorators.curry_uncurry_func import curry_explicit , uncurry_exlicit
+from project.Decorators.curry_uncurry_func import curry_explicit, uncurry_explicit
 
-#tests for curry_explicit
+# tests for curry_explicit
+
 
 def test_curry_explicit_basic():
     """
-    Checking the base case of currying
+    Check the base case of currying
     """
-    def add(a,b,c):
-        return a+b+c
-    
+
+    def add(a, b, c):
+        return a + b + c
+
     curried_add = curry_explicit(add, 3)
-    assert curried_add(1)(2)(3)==6
+    assert curried_add(1)(2)(3) == 6
+
 
 def test_curry_explicit_zero_arity():
     """
-    Checking the case when arity = 0
+    Check the case when arity = 0
     """
+
     def zero_arg_func():
         return 42
-    
-    curried_func = curry_explicit(zero_arg_func , 0)
+
+    curried_func = curry_explicit(zero_arg_func, 0)
     assert curried_func() == 42
+
 
 def test_curry_explicit_zero_arity_error():
     """
     Check for the case where arity = 0, but the function expects an argument.
     """
+
     def func_with_args(x):
         return x
-    
+
     with pytest.raises(ValueError):
-        curry_explicit(func_with_args , 0)
+        curry_explicit(func_with_args, 0)
+
 
 def test_curry_with_negative_arity():
     """
     Check for case when arity is negative
     """
-    def func(a,b):
-        return a+b
-    
+
+    def func(a, b):
+        return a + b
+
     with pytest.raises(ValueError):
-        curry_explicit(func , -1)
+        curry_explicit(func, -1)
+
 
 def test_curry_explicit_too_many_args():
     """
     Check for the case when too many arguments are passed.
     """
-    def func(a,b):
-        return a+b
-    
-    curried_func = curry_explicit(func , 2)
+
+    def func(a, b):
+        return a + b
+
+    curried_func = curry_explicit(func, 2)
     with pytest.raises(TypeError):
         curried_func(1)(2)(3)
+
+
+# test for uncurry_explicit
+
+
+def test_uncurry_explicit_basic():
+    """
+    Check the base case of uncurrying
+    """
+
+    def add(a, b, c):
+        return a + b + c
+
+    curried_add = curry_explicit(add, 3)
+    uncurried_add = uncurry_explicit(curried_add, 3)
+    assert uncurried_add(1, 2, 3) == 6
+
+
+def test_uncurry_explicit_zero_arity():
+    """
+    Check the case when arity = 0
+    """
+
+    def zero_arg_func():
+        return 42
+
+    uncurried_func = uncurry_explicit(zero_arg_func, 0)
+    assert uncurried_func() == 42
+
+
+def test_uncurry_explicit_negative_arity():
+    """
+    Check for error when arity is negative.
+    """
+
+    def func(a, b):
+        return a + b
+
+    with pytest.raises(ValueError):
+        uncurry_explicit(func, -1)
+
+
+def test_uncurry_explicit_wrong_arg_count():
+    """
+    Check for error when number of arguments is incorrect.
+    """
+
+    def add(a, b, c):
+        return a + b + c
+
+    curried_add = curry_explicit(add, 3)
+    uncurried_add = uncurry_explicit(curried_add, 3)
+    with pytest.raises(ValueError):
+        uncurried_add(1, 2)
+
+
+def test_uncurry_not_fully_curried():
+    """
+    Check for an error if a function is not fully curried.
+    """
+
+    def add(a, b):
+        return a + b
+
+    with pytest.raises(TypeError):
+        uncurry_explicit(add, 2)
